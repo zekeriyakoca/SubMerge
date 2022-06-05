6228bea7711bfd709ac51de5bcfb239e03ffa3d4
Zekeriya <zekeriyakocairi@gmail.com>
2022-06-05 16:52:51 +0200

    Function created consuming 2 srt files and returning a result file

---
+++ SubMerge.Engine/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubMerge.Engine.Utils
+{
+    public static class Constants
+    {
+        public static Encoding TrEncoding = Encoding.GetEncoding("ISO-8859-9");
+    }
+}
