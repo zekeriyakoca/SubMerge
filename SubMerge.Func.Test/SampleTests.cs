14464f9a67ed1b9c66508c410c5327c1e49c9ed0
Zekeriya <zekeriyakocairi@gmail.com>
2022-06-06 19:21:12 +0200

    Make sample test passing

--- SubMerge.Func.Test/SampleTests.cs
+++ SubMerge.Func.Test/SampleTests.cs
@@ -5,11 +5,6 @@ namespace SubMerge.Func.Test
     public class SampleTests
     {
         [Fact]
-        public void FailingTest()
-        {
-            Assert.True(false);
-        }
-        [Fact]
         public void PassingTest()
         {
             Assert.True(true);
