8eb48c0e2fb6c88dd3784dc3435162d06187e441
Zekeriya <zekeriyakocairi@gmail.com>
2022-06-06 22:05:49 +0200

    Reorganize file structure

---
+++ Function/SubMerge.Func.Test/ListLogger.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubMerge.Func.Test
+{
+    public class ListLogger : ILogger
+    {
+        public IList<string> Logs;
+
+        public IDisposable BeginScope<TState>(TState state) => NullScope.Instance;
+
+        public bool IsEnabled(LogLevel logLevel) => false;
+
+        public ListLogger()
+        {
+            this.Logs = new List<string>();
+        }
+
+        public void Log<TState>(LogLevel logLevel,
+                                EventId eventId,
+                                TState state,
+                                Exception exception,
+                                Func<TState, Exception, string> formatter)
+        {
+            string message = formatter(state, exception);
+            this.Logs.Add(message);
+        }
+    }
+
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+
+        private NullScope() { }
+
+        public void Dispose() { }
+    }
+}
---
+++ Function/SubMerge.Func.Test/LoggerTypesEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubMerge.Func.Test
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
